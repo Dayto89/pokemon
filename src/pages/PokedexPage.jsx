aecee52271d6b723d98d9d932334cdd43b0ef4b2
Dayto89 <117123949+Dayto89@users.noreply.github.com>
2023-08-10 18:27:58 -0500

    Update PokedexPage.jsx

--- src/pages/PokedexPage.jsx
+++ src/pages/PokedexPage.jsx
@@ -62,9 +62,9 @@ const PokedexPage = () => {
             <input className="pokedex__input" placeholder="Nombre de Pokemon" ref={inputSearch} type="text" />
             <button className="pokedex__button">Search</button>
           </form>
-          <div className="selectype"
+          
             <SelectType setSelectValue={setSelectValue} />
-        </div>
+
         </div>
       </div>
 
